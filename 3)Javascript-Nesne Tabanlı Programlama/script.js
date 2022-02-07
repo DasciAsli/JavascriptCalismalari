045798dd1222f34052330d32ba95dae4f7ae60ee
DasciAsli <aslihandasci1@gmail.com>
2022-02-07 15:36:56 +0300

    Javascript Nesne Tabanlı Programlama-2

--- 3)Javascript-Nesne Tabanlı Programlama/script.js
+++ 3)Javascript-Nesne Tabanlı Programlama/script.js
@@ -229,3 +229,232 @@
 // head1.Introduce();
 // head1.teach();
 // head1.shareTask();
+
+//PRİMİTİVE & OBJECTS
+// var a = 10;
+// var b = a;
+
+// console.log(a);
+// console.log(b);
+
+// var obj1 = {
+//   name: "A",
+//   age: 1,
+// };
+
+// var obj2 = obj1;
+// obj2.age = 2; //Bu işlemi yaptığımızda obj1in de age'i 2 olur.
+// //Çünkü primitive tiplerde stackde ayrı ayrı yerlerde tutulur veriler ama objectlerde aynı adreste tutulur.
+
+// console.log(obj1);
+// console.log(obj2);
+
+/******************************/
+
+// var num = 50;
+// var account = {
+//   name: "Yigit",
+//   accountNumber: "123456",
+// };
+
+// function update(a, b) {
+//   a = 100;
+//   b.accountNumber = "22222";
+// }
+
+// update(num, account);
+
+// /*Burası önemli!Primitive değişkenlere stack üzerinden direkt ulaşabiliyoruz ancak reference tipli değişkenler heap üzerinde yer aldığı için direkt ulaşamıyoruz.Bunun için stackde bir reference belirlememiz gerekiyor ve bu reference yardımıyla heape ulaşabiliyoruz.Function burada bir object.
+// update(num,account) dediğimiz zaman update fonksiyonunu çalıştırıyoruz ve burada update'in parametresi olan num'a (Bu num stackdeki num ile aynı değil-kopyası gibi düşün) 100degerini aktarmış oluyoruz.Yani primitive tipteki num'ın değeri değişmiyor.ancak account'ın accountNumber değişkenini değiştirerek adresteki account değişkeninin içeriğini değiştirmiş oluyoruz.*/
+// console.log(account.name);
+// console.log(account.accountNumber);
+// console.log(num);
+
+/******************************/
+
+//CALLBACK FUNCTİONS
+
+// let val;
+// function MultipleByTwo(a, b, c, callback) {
+//   let arr = [];
+//   for (let i = 0; i < 3; i++) {
+//     arr[i] = callback(arguments[i] * 2);
+//   }
+//   return arr;
+// }
+
+// function addOne(a) {
+//   return a + 1;
+// }
+
+// val = MultipleByTwo(1, 2, 3, addOne);
+// // val = addOne(10);
+// // for (var index = 0; index < val.length; index++) {
+// //   val[index] = addOne(val[index]);
+// // }
+
+// console.log(val);
+
+/******************************/
+
+//IMMEDIATE FUNCTİONS
+//Yazdığımız fonksiyonun sadece programın başında bir kere çalışmasını istersek immediate functionsları kullanmamız gerekir.
+
+// (function welcome(name) {
+//   var days = [
+//     "Sunday",
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday",
+//   ];
+//   var today = new Date();
+//   var message = `Welcome ${name} Today is ${days[today.getDay()]}`;
+//   console.log(message);
+// })("Ayşe");
+
+/******************************/
+
+//FONKSİYONDAN FONKSİYON DÖNDÜRME
+
+// function Question(hobby) {
+//   switch (hobby) {
+//     case "Car":
+//       return function (name) {
+//         console.log(`${name}, Do you have a car?`);
+//       };
+//       break;
+//     case "Book":
+//       return function (name) {
+//         console.log(`${name} , What is your favourite book?`);
+//       };
+//       break;
+//     case "Software":
+//       return function (name) {
+//         console.log(`${name} , Are you interested in asp.net?`);
+//       };
+//       break;
+//     default:
+//       return function (name) {
+//         console.log(`${name} , How are you?`);
+//       };
+//       break;
+//   }
+// }
+
+// var softwareQuestion = Question("Software");
+// softwareQuestion("Asli");
+
+// var bookQuestion = Question("Book");
+// bookQuestion("Evrim");
+
+/******************************/
+
+//GETTER & SETTER
+
+//Uzun yöntem
+// const person = {
+//   firstName: "Sadık",
+//   lastName: "Turan",
+//   getFullName: function () {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+//   setFullName: function (value) {
+//     const parts = value.split(" ");
+//     this.firstName = parts[0];
+//     this.lastName = parts[1];
+//   },
+// };
+
+//Kısa yöntem
+// var person = {
+//   firstName: "Sadık",
+//   lastName: "Turan",
+//   // get FullName() {
+//   //   return `${this.firstName} ${this.lastName}`;
+//   // },
+//   // set FullName(value) {
+//   //   const parts = value.split(" ");
+//   //   this.firstName = parts[0];
+//   //   this.lastName = parts[1];
+//   // },
+// };
+
+// //getter ve setterları eklemenin başka bir yöntemi
+// Object.defineProperty(person, "fullName", {
+//   writable: true, //set methodunun yazdırılabilir olması için
+//   get function() {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+//   set function(value) {
+//     const parts = value.split(" ");
+//     this.firstName = parts[0];
+//     this.lastName = parts[1];
+//   },
+// });
+
+// person.fullName = "Sena Turan";
+// console.log(person.fullName);
+
+/******************************/
+//CALL,APPLY &BİND
+
+// var welcome = function () {
+//   console.log("Welcome " + this.name);
+// };
+// var yigit = { name: "Yigit" };
+// var sema = { name: "Sema" };
+// welcome();
+// welcome.call(sema);
+// welcome.apply(yigit);
+// //apply ve call methodunun kullanımı aynıdır
+
+// welcomeYigit = welcome.bind(yigit); //bind bir fonksiyon döndürür
+// welcomeYigit();
+
+//CALL VE APPLY FARKI:Bunların farkı parametre kullanımlarındadır
+
+// var welcome = function (a, b) {
+//   console.log(
+//     "Welcome " + this.name + ".Are you interested in " + a + " and " + b
+//   );
+// };
+// var yigit = { name: "Yigit" };
+// var sema = { name: "Sema" };
+
+// welcome.call(sema, "asp.net", "angular");
+// welcome.apply(sema, ["asp.net", "angular"]);
+
+// welcomeYigit = welcome.bind(yigit);
+// welcomeYigit("C#", "Mysql");
+
+//CALL&APPLY &BİND UYGULAMA
+//NUMERİC RANGE
+//Fonksiyona gönderdiğimiz herhangi bir sayının belirlediğimiz değerler içinde olup olmadığını kontrol eden uygulama
+// var checkNumber = function (a) {
+//   var count = 0;
+//   this.dizi.forEach(function (item) {
+//     if (item === a) {
+//       count++;
+//     }
+//   });
+//   if (count > 0) {
+//     console.log(`${a} dizide bulunmaktadır`);
+//   } else {
+//     console.log(`${a} dizide bulunmamaktadır`);
+//   }
+// };
+
+// var dizi1 = { dizi: [1, 2, 3, 15, 100] };
+
+// //call ile kullanımı
+// checkNumber.call(dizi1, 20);
+
+// //apply ile kullanımı
+// checkNumber.apply(dizi1, [15]);
+
+// //bind ile kullanımı
+// var checkFunction = checkNumber.bind(dizi1);
+// checkFunction(3);
